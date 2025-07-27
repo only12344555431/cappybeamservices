9f1a6365f01a9332c83cfefe4891443abbb396f2
only12344555431 <139345363+only12344555431@users.noreply.github.com>
2025-07-27 23:23:38 +0300

    Update pnl site v2.py

--- pnl site v2.py
+++ pnl site v2.py
@@ -329,8 +329,10 @@ def api_query():
     except Exception as e:
         return jsonify(error=f"API isteği başarısız: {str(e)}"), 500
 
+import os
+
 if __name__ == "__main__":
-    import os
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    print(f"[*] Flask app başlatılıyor... http://0.0.0.0:{port} adresini açınız.")
+    app.run(debug=False, host="0.0.0.0", port=port)
 
